eca558c54a1eea8b19f16e88e559908bf44c0d46
Whitney15 <114629880+Whitney15@users.noreply.github.com>
2023-07-25 10:46:35 -0500

    set up seeds file

--- db/seeds.sql
+++ db/seeds.sql
@@ -1,2 +1,12 @@
-
+INSERT INTO department (name)
+VALUES  (""),
+        ("");
        
+
+INSERT INTO role (title, salary, deparment_id)
+VALUES (""),
+        ("");
+
+INSERT INTO employee (first_name, last_name, role_id, manager_id)
+VALUES (""),
+        ("");
