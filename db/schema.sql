3caeb0fc7081f5cfb1bd8445287f90ab8a20766c
Whitney15 <114629880+Whitney15@users.noreply.github.com>
2023-07-27 17:03:09 -0500

    updated serer.js file

--- db/schema.sql
+++ db/schema.sql
@@ -10,12 +10,12 @@ USE employees_db;
 -- Creates the table "department" within employees_db --
 CREATE TABLE department (
   id INT NOT NULL,
-  name VARCHAR(30),
+  department_name VARCHAR(30),
   PRIMARY KEY (id)
 );
 
 CREATE TABLE role (
-  id INT,
+  id INT NOT NULL,
   title VARCHAR(30),
   salary DECIMAL,
   department_id INT,
@@ -23,7 +23,7 @@ CREATE TABLE role (
 );
 
 CREATE TABLE employee (
-  id INT,
+  id INT NOT NULL,
   first_name VARCHAR(30),
   last_name VARCHAR(30),
   role_id INT,
--- db/seeds.sql
+++ db/seeds.sql
@@ -1,12 +1,20 @@
-INSERT INTO department (name)
-VALUES  (""),
-        ("");
+USE employees_db;
+
+INSERT INTO department (department_name)
+VALUES  ("Human Resources"),
+        ("Engineering"),
+        ("Sales"),
+        ("Marketing");
        
 
 INSERT INTO role (title, salary, deparment_id)
-VALUES (""),
-        ("");
+VALUES ("HR Manager", 70000, 1),
+       ("Engineer", 80000, 2),
+       ("Sales Executive", 60000, 3),
+       ("Marketing Specialist", 65000, 4);
 
 INSERT INTO employee (first_name, last_name, role_id, manager_id)
-VALUES (""),
-        ("");
+VALUES ("John", "Doe", 1, NULL),
+       ("Jane", "Smith", 2, 1),
+       ("Emily", "Johnson", 3, 1),
+       ("Mike", "Brown", 4, 1);
